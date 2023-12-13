cfdd020294a2f5945d86306672c5c9145e140d81
Girts Strazdins <girts.strazdins@gmail.com>
2023-12-13 21:40:28 +0100

    (WIP) Day 13, Part 2. Still not right.

--- src/main/java/problem/day13/MirrorMap.java
+++ src/main/java/problem/day13/MirrorMap.java
@@ -1,17 +1,24 @@
 package problem.day13;
 
 import tools.StringGrid;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MirrorMap {
   private static final char ROCK = '#';
+  private static final int MAX_BITS = 20;
   private final StringGrid grid;
   private long[] rows;
   private long[] columns;
+  private final static Set<Long> bits = new HashSet<>();
 
   public MirrorMap(StringGrid grid) {
     this.grid = grid;
     convertRowsToNumbers();
     convertColumnsToNumbers();
+    if (bits.isEmpty()) {
+      initializeBits();
+    }
   }
 
   private void convertColumnsToNumbers() {
@@ -31,10 +38,18 @@ public class MirrorMap {
   private long convertToNumber(String s) {
     long n = 0;
     for (int i = 0; i < s.length(); ++i) {
-      long bit = s.charAt(i) == ROCK? 1 : 0;
+      long bit = s.charAt(i) == ROCK ? 1 : 0;
       n = (n << 1) + bit;
     }
     return n;
+  }
+
+  private void initializeBits() {
+    long bit = 1;
+    for (int i = 0; i < MAX_BITS; ++i) {
+      bits.add(bit);
+      bit <<= 1;
+    }
   }
 
   public long findSymmetryCenter(long[] numbers, boolean expectSmudge) {
@@ -58,14 +73,25 @@ public class MirrorMap {
       long leftNumber = numbers[split - i];
       long rightNumber = numbers[split + i - 1];
       symmetrical = (leftNumber == rightNumber);
-      if (!symmetrical && smudgeExpected) {
-        // Smudge found, ignore it once
+      if (!symmetrical && smudgeExpected && onlyOneBitDifference(leftNumber, rightNumber)) {
+        // Smudge found, ignore it once, remember that this bit was smudged
         symmetrical = true;
         smudgeExpected = false;
       }
       i++;
     }
     return symmetrical && !smudgeExpected;
+  }
+
+  /**
+   * Returns true if numbers n1 and n2 differ only by a single bit.
+   *
+   * @param n1 The first number
+   * @param n2 The second number
+   * @return True if n1 and n2 differ only by a single bit
+   */
+  private boolean onlyOneBitDifference(long n1, long n2) {
+    return bits.contains(Math.abs(n2 - n1));
   }
 
   public long findSymmetryScore(boolean expectSmudge) {
